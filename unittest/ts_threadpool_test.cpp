2677e2940daf691ac8e5629a92ebe5aeae78c2a7
Niroj Pokhrel <nirojpokhrel@gmail.com>
2019-06-02 15:38:26 +0300

    Added a unit-test for testing parallel accumulate

--- unittest/ts_threadpool_test.cpp
+++ unittest/ts_threadpool_test.cpp
@@ -1,6 +1,9 @@
-#include <future>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <future>
+#include <vector>
 #include "src/simple_tpool.h"
 
 TEST (SimpleThreadPool, SubmitDifferentTask) {
@@ -18,6 +21,28 @@ TEST (SimpleThreadPool, SubmitDifferentTask) {
   std::this_thread::sleep_for(std::chrono::milliseconds(100));  // let child threads complete
   EXPECT_EQ(e, 50);
   EXPECT_EQ(f, 300);
+}
+
+TEST (SimpleThreadPool, ParallelAccumulateTenElem) {
+  std::vector<int> input_vec;
+  int num_of_elem = 100;    // Set it multiple of 10
+  input_vec.reserve(num_of_elem);
+  for (int i=0; i<num_of_elem; i++) {
+    input_vec.emplace_back(i);
+  }
+  auto Addition = [] (const std::vector<int>& input_vec, int begin, int end, int& sum) -> void {
+    for (int i=begin; i<end; ++i) {
+      sum += input_vec[i];
+    }
+  };
+  std::vector<int> result(10, 0.0);
+  thread_pool::ThreadPool tpool;
+  for (int i=0; i<num_of_elem; i+= 10) {
+    tpool.SubmitWork(std::bind(Addition, std::ref(input_vec), i, i+10, std::ref(result[i/10])));
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  int total_sum = std::accumulate(result.begin(), result.end(), 0);
+  EXPECT_EQ(total_sum, (num_of_elem - 1) * num_of_elem / 2);  // First elem is always zero so discaring the first elem
 }
 
 int main(int argc, char **argv) {
